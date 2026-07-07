9884b0d742d9b653971b98c6c3e25d40656acced
Maor Levi <maor@optivst.com>
2026-07-07 18:22:40 +0300

    fix push.bat script

--- push.bat
+++ push.bat
@@ -3,8 +3,8 @@ cd /d "%~dp0"
 
 git add -A
 
-git diff --cached --quiet
-if %errorlevel% == 0 (
+git status --short > "%TEMP%\gitstatus.txt" 2>&1
+for %%A in ("%TEMP%\gitstatus.txt") do if %%~zA==0 (
   echo Nothing to commit.
   pause
   exit /b 0
@@ -12,17 +12,15 @@ if %errorlevel% == 0 (
 
 echo.
 echo Changes to commit:
-git status --short
+type "%TEMP%\gitstatus.txt"
 echo.
 
-set /p MSG="Commit message (leave blank for timestamp): "
-if "%MSG%"=="" (
-  for /f "tokens=1-5 delims=/: " %%a in ("%date% %time%") do set MSG=update %%a-%%b-%%c %%d:%%e
-)
+set /p MSG="Commit message: "
+if "%MSG%"=="" set MSG=update
 
 git commit -m "%MSG%"
 git push
 
 echo.
-echo Done! Check Vercel for deployment.
+echo Done!
 pause
